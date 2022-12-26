773cb4c907d312f3a7a245ceef8b920fa22e4276
Mike Onofrienko <omvmike@gmail.com>
2022-12-26 23:18:27 +0200

    update custom path

--- index.js
+++ index.js
@@ -10,23 +10,33 @@ const policyStatements = [{
   ]
 }];
 
+let policies = policyStatements;
+
 class SimplifyDefaultExecRole {
   constructor(serverless) {
-    this.policy = (serverless.service.custom && serverless.service.custom.iamRoleLambdaExecutionPolicy) || policyStatements;
+    if (
+      serverless.service.custom &&
+      serverless.service.custom.simplifyDefaultExecRole &&
+      serverless.service.custom.simplifyDefaultExecRole.policies &&
+      Array.isArray(serverless.service.custom.simplifyDefaultExecRole.policies)
+    ) {
+      policies.push(...serverless.service.custom.simplifyDefaultExecRole.policies);
+    }
+
     this.hooks = {
       'before:package:finalize': function() {
-        simplifyBaseIAMLogGroups(serverless, this.policy);
+        simplifyBaseIAMLogGroups(serverless);
       }
     };
   }
 }
 
-function simplifyBaseIAMLogGroups(serverless, policy) {
+function simplifyBaseIAMLogGroups(serverless) {
   const resourceSection = serverless.service.provider.compiledCloudFormationTemplate.Resources;
 
   for (const key in resourceSection) {
     if (key === 'IamRoleLambdaExecution') {
-      resourceSection[key].Properties.Policies[0].PolicyDocument.Statement = policy;
+      resourceSection[key].Properties.Policies[0].PolicyDocument.Statement = policies;
     }
   }
 }
